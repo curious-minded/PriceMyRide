d898ae7bdce71fd5e8ecae5addd66e64257c1143
curious-minded <dakshsaxena17062004@gmail.com>
2024-09-29 09:55:55 +0530

    Update auth1.py

--- Regression project/auth1.py
+++ Regression project/auth1.py
@@ -126,13 +126,13 @@ def login_page():
                 st.error('Passwords do not match. Please try again')
             else:
                  all_users_ref = db.reference('users')
-                all_users = all_users_ref.get()
+                 all_users = all_users_ref.get()
                 
-                if all_users:
-                    existing_handles = [user_data['handle'] for user_data in all_users.values() if 'handle' in user_data]
-                    if handle in existing_handles:
-                        st.error(f"The handle '{handle}' is already taken. Please choose another one.")
-                        return
+                 if all_users:
+                     existing_handles = [user_data['handle'] for user_data in all_users.values() if 'handle' in user_data]
+                     if handle in existing_handles:
+                         st.error(f"The handle '{handle}' is already taken. Please choose another one.")
+                         return
                 try:
                     user = auth.create_user(
                         email=email,
